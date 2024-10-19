95dd27d1ef6af4bb0077184e274a2c0d8dfdb227
vrhernandez <vicenterh99@gmail.com>
2024-10-18 20:20:44 -0700

    added functionality to both buttons to update display buttons

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 // temp data, will be added to local storage
 const watchedVideos = []
+let watchedToday = 0
 let totalWatched = 0
+let projectsCompleted = 0
 
 const watchedVideoBtnEl = document.getElementById("watched-video-btn")
+const projectCompleteBtnEl = document.getElementById("project-complete-btn")
+const totalProjectsEl = document.getElementById("total-projects")
+const overallProgLabelEl = document.getElementById("overall-prog-label")
+
 
 watchedVideoBtnEl.addEventListener("click", function(){
+    watchedToday++
     totalWatched++
-    console.log(`You watched a new video :) Total watched: ${totalWatched}`)
-    document.getElementById("videos-watched-el").innerText = totalWatched
+    document.getElementById("watched-today").innerText = watchedToday
+    document.getElementById("videos-watched").innerText = totalWatched
+    overallProgLabelEl.innerText = `Overall Progress - ${totalWatched+projectsCompleted}`
+    document.getElementById("overall-progress").innerText += "⏩"
+})
+
+projectCompleteBtnEl.addEventListener("click", function(){
+    projectsCompleted++
+    document.getElementById("total-projects").innerText = projectsCompleted
+    overallProgLabelEl.innerText = `Overall Progress - ${totalWatched+projectsCompleted}`
+    document.getElementById("overall-progress").innerText += "🏅"
 })
